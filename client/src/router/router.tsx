c69c9a8909086de7a737d6e1d87e691be325ecfb
zpran <zanna@prane.org>
2023-09-18 22:31:54 +0100

    feat: code 404 page

--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -2,11 +2,14 @@ import {Routes, Route} from 'react-router-dom';
 import { Home } from '../pages/home';
 import { Misdemeanours } from '../pages/misdemeanours';
 import { ConfessToUs } from '../pages/confess-to-us';
+import { Page404 } from '../pages/home copy';
+
 const Router: React.FC = () => (
     <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/misdemeanours' element={<Misdemeanours/>}/>
         <Route path='/confess-to-us' element={<ConfessToUs/>}/>
+        <Route path='*' element={<Page404/>} />
     </Routes>
 );
 export default Router;
