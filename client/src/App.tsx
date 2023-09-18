9446f6addbaca4e606290f252b1b6b85d2ee1c11
zpran <zanna@prane.org>
2023-09-18 22:49:24 +0100

    feat: navigation component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import Router from './router/router'
+import Nav from './router/nav-link'
 
 function App() {
   const [count, setCount] = useState(0)
   
 
   return (
-    
     <>
     <BrowserRouter>
+      <Nav />
       <Router />
     </BrowserRouter>
     </>
