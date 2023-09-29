95c73f09c5d5bfd8512ca13ec8c570be63eaa4ab
zpran <zanna@prane.org>
2023-09-30 00:27:19 +0300

    feat: misdemeanours fetching
    output is not ready yet

--- client/src/pages/misdemeanours.tsx
+++ client/src/pages/misdemeanours.tsx
@@ -1,1 +1,26 @@
-export const Misdemeanours : React.FC = () => <>Misdemeanours!</>;
+import {useEffect, useState} from "react";
+import {Misdemeanour} from "../types/misdemeanours.types";
+
+export const Misdemeanours = () => {
+    const [misdemeanours, setMisdemeanours] = useState<Misdemeanour[]>([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/misdemeanours/10")
+            .then(response => response.json())
+            .then((data:Misdemeanour[]) => setMisdemeanours(data))
+            .catch((error) => console.error("Error fetching data:", error));
+    }, [])
+    console.log(misdemeanours)
+
+    return (
+        <div>
+            {misdemeanours?.map((misdemeanour) => (
+                <div key={misdemeanour?.citizenId}>
+                    <p>Citizen ID: {misdemeanour?.citizenId}</p>
+                    <p>Misdemeanour: {misdemeanour?.misdemeanour}</p>
+                    <p>Date: {misdemeanour?.date}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
