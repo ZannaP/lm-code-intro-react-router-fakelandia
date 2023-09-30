4a9ef612c1ac830cfb8025e3274887b192621990
zpran <zanna@prane.org>
2023-09-30 23:01:35 +0300

    feat: random image for punishment

--- client/src/pages/misdemeanours.tsx
+++ client/src/pages/misdemeanours.tsx
@@ -3,8 +3,9 @@ import {Misdemeanour} from "../types/misdemeanours.types";
 
 export const Misdemeanours = () => {
     const [misdemeanours, setMisdemeanours] = useState<Misdemeanour[]>([]);
+    const [randomImage, setRandomImage] = useState<string>('https://fastly.picsum.photos/id/690/150/100.jpg?hmac=CShVFo9KmPvqR4nAO1h1MmnPkS9oCZlx1xD4LiCqfBU')
     useEffect(() => {
-        fetch("http://localhost:8080/api/misdemeanours/10")
+        fetch("http://localhost:8080/api/misdemeanours/5")
             .then(response => response.json())
             .then((data) => {
                 setMisdemeanours(data.misdemeanours);
@@ -13,20 +14,26 @@ export const Misdemeanours = () => {
 
     return (
         <div className={"the-container"}>
-            <div className={"grid grid-cols-4 gap-8"}>
+            <div className={"grid grid-cols-4 gap-8 p-6"}>
                 <div>Citizen ID</div>
                 <div>Misdemeanour</div>
                 <div>Date</div>
                 <div>Punishment idea</div>
             </div>
-            {misdemeanours?.map((misdemeanour) => (
-                <div key={misdemeanour?.citizenId} className={"grid grid-cols-4 gap-8"}>
-                    <div>{misdemeanour?.citizenId}</div>
-                    <div>{misdemeanour?.misdemeanour}</div>
-                    <div>{misdemeanour?.date}</div>
-                    <div>x</div>
-                </div>
-            ))}
+            {misdemeanours?.map((misdemeanour) => {
+                const randomImageURL = `https://picsum.photos/150/100?random=${Math.random()}`;
+
+                return (
+                    <div key={misdemeanour?.citizenId} className={"grid grid-cols-4 gap-8 p-6"}>
+                        <div>{misdemeanour?.citizenId}</div>
+                        <div>{misdemeanour?.misdemeanour}</div>
+                        <div>{misdemeanour?.date}</div>
+                        <div>
+                            <img src={randomImageURL} alt={`${misdemeanour?.misdemeanour} punishment`} />
+                        </div>
+                    </div>
+                );
+            })}
         </div>
     );
 
