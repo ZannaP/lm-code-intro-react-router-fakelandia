08d15426f9bd229d9062bc395719dfdac16bee18
zpran <zanna@prane.org>
2023-09-30 22:29:37 +0300

    feat: misdemeanours list output

--- client/src/pages/misdemeanours.tsx
+++ client/src/pages/misdemeanours.tsx
@@ -3,24 +3,31 @@ import {Misdemeanour} from "../types/misdemeanours.types";
 
 export const Misdemeanours = () => {
     const [misdemeanours, setMisdemeanours] = useState<Misdemeanour[]>([]);
-
     useEffect(() => {
         fetch("http://localhost:8080/api/misdemeanours/10")
             .then(response => response.json())
-            .then((data:Misdemeanour[]) => setMisdemeanours(data))
-            .catch((error) => console.error("Error fetching data:", error));
+            .then((data) => {
+                setMisdemeanours(data.misdemeanours);
+            }).catch((error) => console.error("Error fetching data:", error));
     }, [])
-    console.log(misdemeanours)
 
     return (
-        <div>
+        <div className={"the-container"}>
+            <div className={"grid grid-cols-4 gap-8"}>
+                <div>Citizen ID</div>
+                <div>Misdemeanour</div>
+                <div>Date</div>
+                <div>Punishment idea</div>
+            </div>
             {misdemeanours?.map((misdemeanour) => (
-                <div key={misdemeanour?.citizenId}>
-                    <p>Citizen ID: {misdemeanour?.citizenId}</p>
-                    <p>Misdemeanour: {misdemeanour?.misdemeanour}</p>
-                    <p>Date: {misdemeanour?.date}</p>
+                <div key={misdemeanour?.citizenId} className={"grid grid-cols-4 gap-8"}>
+                    <div>{misdemeanour?.citizenId}</div>
+                    <div>{misdemeanour?.misdemeanour}</div>
+                    <div>{misdemeanour?.date}</div>
+                    <div>x</div>
                 </div>
             ))}
         </div>
     );
+
 }
